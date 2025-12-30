464e81de218d1a71b811ffed73aa08b8a648c210
Explosion247 <Dave.staward@gmail.com>
2025-12-30 19:37:29 +0000

    - added cloudinary - added images to Event model and rearranged - added db image to cards in index

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -10,12 +11,13 @@ class Event(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
-    status = models.IntegerField(choices=STATUS, default=0)
+    image = CloudinaryField('image', default='placeholder')
+    address = models.TextField()
+    lat = models.FloatField("latitude", blank=True, null=True)
+    long = models.FloatField("longtitude", blank=True, null=True)
     liked_by = models.ManyToManyField(
         User,
         related_name='liked_event',
         blank=True
     )
-    lat = models.FloatField("latitude", blank=True, null=True)
-    long = models.FloatField("longtitude", blank=True, null=True)
-    address = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
