649c2a9c7538544264586b913f562434a7ee289c
Explosion247 <Dave.staward@gmail.com>
2025-12-30 11:07:58 +0000

    - added basic html to base.html - added url for index

--- ticketSite/settings.py
+++ ticketSite/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+AUTH_DIRS = os.path.join(BASE_DIR, 'templates', 'allauth')
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'events'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +63,10 @@ ROOT_URLCONF = 'ticketSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+            BASE_DIR
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
